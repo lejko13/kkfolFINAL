fd6cccae3833469c57234301abc0c7c1b77da906
Leo Fudaly <lejko13@users.noreply.github.com>
2026-05-16 23:15:16 +0200

    cdas

--- src/components/landing/Hero.jsx
+++ src/components/landing/Hero.jsx
@@ -9,14 +9,14 @@ export default function Hero() {
   const scrollTo = (id) => document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
 
   const obrazok = '/mainhalvny.png'
-  const oprava = '/public/mobilmain.webp'
+  const oprava = '/mobilmain.webp'
 
   const isDesktop = useMediaQuery({
   query: "(min-width: 700px)",
 });
 
   return (
-    <section id="hero" className="relative min-h-screen flex items-center justify-center overflow-hidden">
+    <section id="hero" className="relative min-h-[750px] md:min-h-screen flex items-center justify-center overflow-hidden">
       {/* Background */}
       <div className="absolute inset-0" style={{ background: '#1A1B1E' }}>
        
